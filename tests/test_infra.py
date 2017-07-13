a933eec1b0ce3d92e655aa9392f4d21a74f4b9af
Fernando González <fernando.gonzalez@opensolutions.io>
2017-07-13 16:42:23 +0200

    Fixed ansible-list warning and added testinfra tests

--- tests/test_infra.py
+++ tests/test_infra.py
@@ -1,26 +1,19 @@
-import pytest
+def test_grafana_server_running_and_enabled(Command, Service):
+    # Check that docker service is running and enabled
+    docker_service = Service("docker")
+    assert docker_service.is_running
+    assert docker_service.is_enabled
+    # Check that grafana service is running and enabled
+    grafana_service = Service("grafana")
+    assert grafana_service.is_running
+    assert grafana_service.is_enabled
 
-def test_grafana_server_running_and_enabled(Command,Service):
-  # Check that docker service is running and enabled
-  docker_service = Service("docker")
-  assert docker_service.is_running
-  assert docker_service.is_enabled
-  # Check that grafana service is running and enabled
-  grafana_service = Service("grafana")
-  assert grafana_service.is_running
-  assert grafana_service.is_enabled
 
-  # Check that grafana-cli ping
-  grafana_output = Command.check_output("docker exec grafana grafana-cli ping")
-  assert grafana_output == "PONG"
-
-def test_grafana_start_stop(Command,Service):
-  Command.run_expect([0],"systemctl stop grafana")
-  grafana_service = Service("grafana")
-  assert not grafana_service.is_running
-  Command.run_expect([0],"systemctl start grafana")
-  assert grafana_service.is_running
-  Command.run_expect([0],"systemctl restart grafana")
-  assert grafana_service.is_running
-  grafana_output = Command.check_output("docker exec grafana grafana-cli ping")
-  assert grafana_output == "PONG"
+def test_grafana_start_stop(Command, Service):
+    Command.run_expect([0], "systemctl stop grafana")
+    grafana_service = Service("grafana")
+    assert not grafana_service.is_running
+    Command.run_expect([0], "systemctl start grafana")
+    assert grafana_service.is_running
+    Command.run_expect([0], "systemctl restart grafana")
+    assert grafana_service.is_running
